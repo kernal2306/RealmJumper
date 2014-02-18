d0630d5289eecb4dc0867824874a49428bb0f96a
Tyler Powell <tylerpowell89@gmail.com>
2014-02-18 11:26:04 -0800

    "created look in image manager"

--- src/gamePack/gfx/ImageManager.java
+++ src/gamePack/gfx/ImageManager.java
@@ -5,6 +5,7 @@ import java.awt.image.BufferedImage;
 public class ImageManager 
 {
 	public BufferedImage player;
+	public int size;
 	
 	public ImageManager(SpriteSheet ss)
 	{
@@ -12,6 +13,11 @@ public class ImageManager
 		SpriteSheet objects[];
 		player = ss.crop(0, 0, 32, 32);
 		
+		
+		for (int i = 0; i <= size; i++)
+		{
+			
+		}
 		
 	}
 
