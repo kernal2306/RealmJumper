3e6e8149817b9b37e05e5a4cb70011c7a0974189
Tyler Powell <tylerpowell89@gmail.com>
2014-06-02 16:22:29 -0700

    created my own rect class for creating objects of rectangles

---
+++ src/gamePack/entities/CollRect.java
@@ -0,0 +1,19 @@
+package gamePack.entities;
+
+public class CollRect 
+{
+	public int width = 0;
+	public int height = 0;
+	public int x = 0, y = 0;
+	
+	public CollRect(int startX, int startY, int width, int height)
+	{
+		this.height = height;
+		this.width = width;
+		this.x = startX;
+		this.y = startY;
+	}
+	
+	
+	
+}
