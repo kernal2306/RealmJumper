673ae5048ff360a3caa4e4b85d0a4b469e8e9546
Tyler Powell <tylerpowell89@gmail.com>
2014-06-03 00:08:50 -0700

    working on new collision detection system

--- src/gamePack/entities/CollRect.java
+++ src/gamePack/entities/CollRect.java
@@ -7,6 +7,10 @@ public class CollRect
 	public int width = 0;
 	public int height = 0;
 	public int x = 0, y = 0;
+	public Line2D top = new Line2D.Float();
+	public Line2D bottom = new Line2D.Float();
+	public Line2D left = new Line2D.Float();
+	public Line2D right = new Line2D.Float();
 	
 	public CollRect(int startX, int startY, int width, int height)
 	{
@@ -18,7 +22,26 @@ public class CollRect
 	
 	public Line2D top()
 	{
-		
+		top.setLine(0, 0, 0, 0);
+		return top;
+	}
+	
+	public Line2D bottom()
+	{
+		bottom.setLine(0, 0, 0, 0);
+		return bottom;
+	}
+	
+	public Line2D left()
+	{
+		top.setLine(0, 0, 0, 0);
+		return left;
+	}
+	
+	public Line2D right()
+	{
+		top.setLine(0, 0, 0, 0);
+		return right;
 	}
 	
 }
